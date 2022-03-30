65a0ce11fed03623974b1301da59fc0b13ed1c5a
Emir Karpuz <emir.karpuz@agito.com.tr>
2022-03-30 23:55:30 +0300

    Kullanıcı Ekle formu oluşturuldu

---
+++ OgrenciBilgiSistemi/FrmKullaniciEkle.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace OgrenciBilgiSistemi
+{
+    partial class FrmKullaniciEkle
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnIptalEt = new System.Windows.Forms.Button();
+            this.PnlSifreTekrar = new System.Windows.Forms.Panel();
+            this.TxtSifreTekrar = new System.Windows.Forms.TextBox();
+            this.PnlSifre = new System.Windows.Forms.Panel();
+            this.TxtSifre = new System.Windows.Forms.TextBox();
+            this.PnlKullaniciEkle = new System.Windows.Forms.Panel();
+            this.BtnOnayla = new System.Windows.Forms.Button();
+            this.LblSifreTekrar = new System.Windows.Forms.Label();
+            this.LblSifre = new System.Windows.Forms.Label();
+            this.LblKullaniciAdi = new System.Windows.Forms.Label();
+            this.TxtKullaniciAdi = new System.Windows.Forms.TextBox();
+            this.LblKullaniciEkle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // BtnIptalEt
+            // 
+            this.BtnIptalEt.FlatAppearance.BorderColor = System.Drawing.Color.Gainsboro;
+            this.BtnIptalEt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnIptalEt.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BtnIptalEt.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(29)))), ((int)(((byte)(52)))));
+            this.BtnIptalEt.Location = new System.Drawing.Point(23, 219);
+            this.BtnIptalEt.Name = "BtnIptalEt";
+            this.BtnIptalEt.Size = new System.Drawing.Size(210, 51);
+            this.BtnIptalEt.TabIndex = 22;
+            this.BtnIptalEt.Text = "İptal Et";
+            this.BtnIptalEt.UseVisualStyleBackColor = true;
+            // 
+            // PnlSifreTekrar
+            // 
+            this.PnlSifreTekrar.BackColor = System.Drawing.Color.Black;
+            this.PnlSifreTekrar.Location = new System.Drawing.Point(208, 203);
+            this.PnlSifreTekrar.Name = "PnlSifreTekrar";
+            this.PnlSifreTekrar.Size = new System.Drawing.Size(250, 1);
+            this.PnlSifreTekrar.TabIndex = 20;
+            // 
+            // TxtSifreTekrar
+            // 
+            this.TxtSifreTekrar.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TxtSifreTekrar.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.TxtSifreTekrar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(192)))), ((int)(((byte)(241)))));
+            this.TxtSifreTekrar.Location = new System.Drawing.Point(208, 168);
+            this.TxtSifreTekrar.Name = "TxtSifreTekrar";
+            this.TxtSifreTekrar.PasswordChar = '*';
+            this.TxtSifreTekrar.Size = new System.Drawing.Size(250, 29);
+            this.TxtSifreTekrar.TabIndex = 18;
+            // 
+            // PnlSifre
+            // 
+            this.PnlSifre.BackColor = System.Drawing.Color.Black;
+            this.PnlSifre.Location = new System.Drawing.Point(208, 154);
+            this.PnlSifre.Name = "PnlSifre";
+            this.PnlSifre.Size = new System.Drawing.Size(250, 1);
+            this.PnlSifre.TabIndex = 21;
+            // 
+            // TxtSifre
+            // 
+            this.TxtSifre.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TxtSifre.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.TxtSifre.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(192)))), ((int)(((byte)(241)))));
+            this.TxtSifre.Location = new System.Drawing.Point(208, 119);
+            this.TxtSifre.Name = "TxtSifre";
+            this.TxtSifre.PasswordChar = '*';
+            this.TxtSifre.Size = new System.Drawing.Size(250, 29);
+            this.TxtSifre.TabIndex = 19;
+            // 
+            // PnlKullaniciEkle
+            // 
+            this.PnlKullaniciEkle.BackColor = System.Drawing.Color.Black;
+            this.PnlKullaniciEkle.Location = new System.Drawing.Point(208, 105);
+            this.PnlKullaniciEkle.Name = "PnlKullaniciEkle";
+            this.PnlKullaniciEkle.Size = new System.Drawing.Size(250, 1);
+            this.PnlKullaniciEkle.TabIndex = 17;
+            // 
+            // BtnOnayla
+            // 
+            this.BtnOnayla.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(29)))), ((int)(((byte)(52)))));
+            this.BtnOnayla.FlatAppearance.BorderColor = System.Drawing.Color.Gainsboro;
+            this.BtnOnayla.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnOnayla.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.BtnOnayla.ForeColor = System.Drawing.Color.White;
+            this.BtnOnayla.Location = new System.Drawing.Point(239, 219);
+            this.BtnOnayla.Name = "BtnOnayla";
+            this.BtnOnayla.Size = new System.Drawing.Size(210, 51);
+            this.BtnOnayla.TabIndex = 16;
+            this.BtnOnayla.Text = "Onayla";
+            this.BtnOnayla.UseVisualStyleBackColor = false;
+            // 
+            // LblSifreTekrar
+            // 
+            this.LblSifreTekrar.AutoSize = true;
+            this.LblSifreTekrar.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LblSifreTekrar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(29)))), ((int)(((byte)(52)))));
+            this.LblSifreTekrar.Location = new System.Drawing.Point(70, 168);
+            this.LblSifreTekrar.Name = "LblSifreTekrar";
+            this.LblSifreTekrar.Size = new System.Drawing.Size(132, 27);
+            this.LblSifreTekrar.TabIndex = 15;
+            this.LblSifreTekrar.Text = "Şifre Tekrar";
+            // 
+            // LblSifre
+            // 
+            this.LblSifre.AutoSize = true;
+            this.LblSifre.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LblSifre.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(29)))), ((int)(((byte)(52)))));
+            this.LblSifre.Location = new System.Drawing.Point(144, 119);
+            this.LblSifre.Name = "LblSifre";
+            this.LblSifre.Size = new System.Drawing.Size(58, 27);
+            this.LblSifre.TabIndex = 14;
+            this.LblSifre.Text = "Şifre";
+            // 
+            // LblKullaniciAdi
+            // 
+            this.LblKullaniciAdi.AutoSize = true;
+            this.LblKullaniciAdi.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.LblKullaniciAdi.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(29)))), ((int)(((byte)(52)))));
+            this.LblKullaniciAdi.Location = new System.Drawing.Point(48, 70);
+            this.LblKullaniciAdi.Name = "LblKullaniciAdi";
+            this.LblKullaniciAdi.Size = new System.Drawing.Size(154, 27);
+            this.LblKullaniciAdi.TabIndex = 13;
+            this.LblKullaniciAdi.Text = "Kullanıcı Adı";
+            // 
+            // TxtKullaniciAdi
+            // 
+            this.TxtKullaniciAdi.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TxtKullaniciAdi.Font = new System.Drawing.Font("Century Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.TxtKullaniciAdi.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(192)))), ((int)(((byte)(241)))));
+            this.TxtKullaniciAdi.Location = new System.Drawing.Point(208, 70);
+            this.TxtKullaniciAdi.Name = "TxtKullaniciAdi";
+            this.TxtKullaniciAdi.Size = new System.Drawing.Size(250, 29);
+            this.TxtKullaniciAdi.TabIndex = 12;
+            // 
+            // LblKullaniciEkle
+            // 
+            this.LblKullaniciEkle.AutoSize = true;
+            this.LblKullaniciEkle.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.LblKullaniciEkle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(29)))), ((int)(((byte)(52)))));
+            this.LblKullaniciEkle.Location = new System.Drawing.Point(140, 9);
+            this.LblKullaniciEkle.Name = "LblKullaniciEkle";
+            this.LblKullaniciEkle.Size = new System.Drawing.Size(201, 37);
+            this.LblKullaniciEkle.TabIndex = 11;
+            this.LblKullaniciEkle.Text = "Kullanıcı Ekle";
+            // 
+            // FrmKullaniciEkle
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(481, 290);
+            this.Controls.Add(this.BtnIptalEt);
+            this.Controls.Add(this.PnlSifreTekrar);
+            this.Controls.Add(this.TxtSifreTekrar);
+            this.Controls.Add(this.PnlSifre);
+            this.Controls.Add(this.TxtSifre);
+            this.Controls.Add(this.PnlKullaniciEkle);
+            this.Controls.Add(this.BtnOnayla);
+            this.Controls.Add(this.LblSifreTekrar);
+            this.Controls.Add(this.LblSifre);
+            this.Controls.Add(this.LblKullaniciAdi);
+            this.Controls.Add(this.TxtKullaniciAdi);
+            this.Controls.Add(this.LblKullaniciEkle);
+            this.Font = new System.Drawing.Font("Century Gothic", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "FrmKullaniciEkle";
+            this.Text = "FrmKullaniciEkle";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button BtnIptalEt;
+        private Panel PnlSifreTekrar;
+        private TextBox TxtSifreTekrar;
+        private Panel PnlSifre;
+        private TextBox TxtSifre;
+        private Panel PnlKullaniciEkle;
+        private Button BtnOnayla;
+        private Label LblSifreTekrar;
+        private Label LblSifre;
+        private Label LblKullaniciAdi;
+        private TextBox TxtKullaniciAdi;
+        private Label LblKullaniciEkle;
+    }
+}
